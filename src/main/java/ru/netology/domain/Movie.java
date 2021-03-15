0dc4d3c3ea4d0d2171b30f0cefe1ad1159ef084c
Pankrashin-iqa14 <pankrashin.al@yandex.ru>
2021-03-16 00:20:37 +0400

    manager afisha

--- src/main/java/ru/netology/domain/Movie.java
+++ src/main/java/ru/netology/domain/Movie.java
@@ -8,11 +8,9 @@ import lombok.NoArgsConstructor;
     @NoArgsConstructor
     @AllArgsConstructor
     @Data
-    public class PurchaseItem {
-        private int id;
-        private int productId;
-        private String productName;
-        private int productPrice;
-        private int count;
+    public class Movie {
+        private int movieId;
+        private String movieName;
+
     }
 
